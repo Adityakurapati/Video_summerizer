94aefb5de61a87540f9753dfa1dcf05c4810d2de
HeyProject7 <124274247+HeyProject7@users.noreply.github.com>
2024-03-04 16:20:29 +0530

    glance-3

--- client/src/Test.jsx
+++ client/src/Test.jsx
@@ -3,25 +3,19 @@ import {useState} from 'react'
 const Test =()=>{      
         const [open,setOpen] = useState(false);
         const variants ={
-                visible:{opacity:1,x:300,transition:{type:"spring",stiffess:2000}},
+                visible:(i)=>({opacity:1,
+                        x:300,
+                        transition:{StaggerChildren:0.2}}),
                 hidden:{opacity:0}
         } ;
+        const items=['items','items2','items3','items4']
         return(
                 <div className='course'>
-                <motion.div
-  className="box"
-  initial="hidden"
-  variants={variants}
-  transition={{ duration: 2 }}
-  animate={open? "visible":"hidden"}
-//   animate="visible"
-//   initial={{opacity:0.5,scale:0}}
-//   whileHover={{opacity:1,scale:2}}
-//   whileInView={{opacity:1,scale:2}}
-//   whileTap={{opacity:1,scale:2}}
-  drag
-></motion.div>   
-<button onClick={()=>setOpen(prev=>!prev)}>Click</button>                     
+                <motion.ul initial="hidden" variants={{variants}} animate="visible">
+                        {items.map((item)=>(
+                        <motion.li variants={variants} key={item} animate="visible">{item}  custom={index}</motion.li>
+                        ))}
+                </motion.ul>                
                 </div>
         )
 }
